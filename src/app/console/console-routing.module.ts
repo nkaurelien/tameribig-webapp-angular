bd89a5759a188df845579ad12f8269e3f6697b1a
NKUMBE “nkaurelien” AURELIEN <nkaurelien@gmail.com>
2019-11-22 04:45:13 +0100

    Update Console Routing

--- src/app/console/console-routing.module.ts
+++ src/app/console/console-routing.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConsoleLayoutComponent } from './console-layout/console-layout.component';
 
 
 const routes: Routes = [
   {
-    path: 'user',
-    loadChildren: () => import('@modules/console/profile/profile.module').then(m => m.ProfileModule)
+    path: '',
+    component: ConsoleLayoutComponent,
+    children: [
+      {
+        path: 'user',
+        loadChildren: () => import('@modules/console/profile/profile.module').then(m => m.ProfileModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('@modules/console/parameters/parameters.module').then(m => m.ParametersModule)
+      },
+      {
+        path: '',
+        redirectTo: 'user',
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: '404',
+        pathMatch: 'full'
+      },
+    ],
   },
   {
-    path: 'settings',
-    loadChildren: () => import('@modules/console/parameters/parameters.module').then(m => m.ParametersModule)
+    path: '**',
+    redirectTo: 'user',
+    pathMatch: 'full'
   },
 ];
 
