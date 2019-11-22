8ec9d67182649ab5bf1c1f00670f43f087acc34c
NKUMBE “nkaurelien” AURELIEN <nkaurelien@gmail.com>
2019-11-22 03:36:50 +0100

    Fix Footer

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { FooterComponent } from './main-layout/footer/footer.component';
 
 
 @NgModule({
-  declarations: [MainLayoutComponent],
+  declarations: [
+    MainLayoutComponent,
+    FooterComponent,
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
