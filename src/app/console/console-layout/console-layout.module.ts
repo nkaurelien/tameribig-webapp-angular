d0e8f162b357a88aa804b31a4c3369df2088f782
nkumbe aurelien <nkaurelien@gmail.com>
2019-12-13 13:43:17 +0100

    Console routing

---
+++ src/app/console/console-layout/console-layout.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationModule } from 'src/app/main/main-layout/navigation/navigation.module';
+import { ConsoleLayoutComponent } from './console-layout.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  // declarations: [ConsoleLayoutComponent, FooterComponent, ],
+  // imports: [
+  //   CommonModule,
+  //   NavigationModule,
+  // ],
+  // exports: [
+    // NavigationModule,
+  //   FooterComponent,
+  //     ConsoleLayoutComponent,
+  // ],
+  // schemas: [ NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA,],
+})
+export class ConsoleLayoutModule { }
--- src/app/console/console.module.ts
+++ src/app/console/console.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConsoleRoutingModule } from './console-routing.module';
-import { ConsoleLayoutComponent } from './console-layout/console-layout.component';
-import { FooterComponent } from './console-layout/footer/footer.component';
-import { NavigationModule } from './console-layout/navigation/navigation.module';
-import { SidenavModule } from 'ng-uikit-pro-standard';
+import { ParametersComponent } from './parameters/parameters.component';
+import { ConsoleLayoutModule } from './console-layout/console-layout.module';
+import {NavigationModule} from './console-layout/navigation/navigation.module';
+import {SidenavModule} from 'ng-uikit-pro-standard';
+import {ConsoleLayoutComponent} from './console-layout/console-layout.component';
+import {FooterComponent} from './console-layout/footer/footer.component';
 
 
 @NgModule({
-  declarations: [ConsoleLayoutComponent, FooterComponent],
-  imports: [
-    CommonModule,
-    ConsoleRoutingModule,
-    NavigationModule,
-    SidenavModule,
-  ]
+    declarations: [ParametersComponent, ConsoleLayoutComponent, FooterComponent],
+    imports: [
+        CommonModule,
+        ConsoleRoutingModule,
+        // ConsoleLayoutModule,
+        NavigationModule,
+        SidenavModule,
+    ],
+    exports: [
+        ConsoleLayoutModule,
+    ],
 })
 export class ConsoleModule { }
