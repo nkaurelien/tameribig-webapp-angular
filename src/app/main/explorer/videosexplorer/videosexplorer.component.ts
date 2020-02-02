c2c96dd21cbf181d5534ccf7c39d9c878495b30e
ngwenidriss@gmail.com <ngwenidriss@gmail.com>
2020-02-02 19:22:52 +0100

    revue de la grille du mansory d'image

--- src/app/main/explorer/videosexplorer/videosexplorer.component.ts
+++ src/app/main/explorer/videosexplorer/videosexplorer.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { dummyPicturesMocks } from '@data/dummy-pictures';
 import {NgxMasonryOptions} from 'ngx-masonry';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-videos-explorer',
   templateUrl: './videosexplorer.component.html',
-  styleUrls: ['./videosexplorer.component.css']
+  styleUrls: [
+    './videosexplorer.component.css',
+    // '~/ng-masonry-grid/ng-masonry-grid.css',
+  ],
+  
+  // encapsulation: ViewEncapsulation.None
 })
 export class VideosExplorerComponent implements OnInit {
   constructor() {}
-
+  displayMode = 'mansory-grid';
+  form: any = <any>{};
   dummyPictures = dummyPicturesMocks;
-
-
   defaultImage = 'assets/images$/default-image.png';
   offset = 100;
   masonryImages;
@@ -21,11 +26,49 @@ export class VideosExplorerComponent implements OnInit {
     transitionDuration: '0.8s'
   };
   ngOnInit() {
+    this.showImages();
+  }
+
+  
+  showImages() {
     this.masonryImages = this.dummyPictures.slice(0, this.limit);
   }
 
+  get searching () {
+    try {
+      return (this.form.keyword || '').length > 0;      
+    } catch (error) {
+      return false;
+    }
+  }
+
   showMoreImages() {
-      this.limit += 15;
-      this.masonryImages = this.dummyPictures.slice(0, this.limit);
+    this.limit += 15;
+    
+      if (this.searching) {
+        this.masonryImages = this.filterImagesByKeyword(this.form.keyword).slice(0, this.limit);
+      } else {
+        this.masonryImages = this.dummyPictures.slice(0, this.limit);
+
+      }
+  }
+
+  filterImagesByKeyword(keyword: string) {
+    return this.dummyPictures.filter(picture => {
+      return  (picture.description || '').toLowerCase().includes(keyword);
+     });
+  }
+
+  search(searchForm: NgForm) {
+    if (searchForm.invalid) {
+      return;
+    }
+    this.masonryImages = this.filterImagesByKeyword(this.form.keyword);
+    
+
+    // this.imageService.searchImages(this.form.keyword, this.page)
+    //   .subscribe((res: any) => {
+    //     this.images = this.images.concat(res.hits);
+    //   })
   }
 }
