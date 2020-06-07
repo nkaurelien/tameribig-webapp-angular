4d5597adf22bf01c4f3f974c07456664ba8f3fd3
nkaurelien <nkaurelien@gmail.com>
2020-06-08 00:10:06 +0100

    FIX: REdimentionner automatiquement la largeur des suggestion de recherche a la largeur du formulaire de recherche penadnt le redimentionnement de la page

--- src/app/main/home/searcher/searcher.component.ts
+++ src/app/main/home/searcher/searcher.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {AfterViewInit, Component, HostListener, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {Observable} from 'rxjs';
 import {debounceTime, map, take} from 'rxjs/operators';
 import {statesWithFlagsMocks} from '@data/states';
+import {DeviceDetectorService} from "ngx-device-detector";
 
-const statesWithFlags: {name: string, flag: string}[] = statesWithFlagsMocks;
+const statesWithFlags: { name: string, flag: string }[] = statesWithFlagsMocks;
 
 @Component({
   selector: 'app-searcher',
@@ -12,23 +13,44 @@ const statesWithFlags: {name: string, flag: string}[] = statesWithFlagsMocks;
   encapsulation: ViewEncapsulation.None
 })
 
-export class SearcherComponent implements OnInit {
+export class SearcherComponent implements OnInit, AfterViewInit {
   public model: any;
+  public searchSuggestionMinWidth = 0;
+  @ViewChild('searchInput', {static: false}) searchInput;
+  @ViewChild('searchForm', {static: false}) searchForm;
 
-  search = (text$: Observable<string>) =>
-  text$.pipe(
-    debounceTime(200),
-    map(term => {
-      return  term === '' ? []
-      : statesWithFlags.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10);
-    }, take(5)),
-  )
-
-formatter = (x: {name: string}) => x.name;
-
-  constructor() {
+  constructor(
+      private deviceService: DeviceDetectorService
+  ) {
 
   }
 
-  ngOnInit() {}
+  search = (text$: Observable<string>) =>
+      text$.pipe(
+          debounceTime(200),
+          map(term => {
+            return term === '' ? []
+                : statesWithFlags.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10);
+          }, take(5)),
+      );
+
+  formatter = (x: { name: string }) => x.name;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+    this.resizeSearchSuggestions();
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.resizeSearchSuggestions();
+  }
+
+  resizeSearchSuggestions(): void {
+    const element = this.searchForm.nativeElement;
+    const searchSuggestionMinWidth = element.clientWidth;
+    this.searchSuggestionMinWidth = searchSuggestionMinWidth;
+  }
 }
